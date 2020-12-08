b17ca1da51cae2ba6cd7bce0608ab709858bb08d
alokcedcoss <74813756+alokcedcoss@users.noreply.github.com>
2020-12-08 13:42:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ $('#u').on('click', '#delete',function(){
     $(this).parent().remove();
 });
 $('#u').on('click', '#check',function(){
-    $(this).parent().appendTo("#com").html("<input type='checkbox' id='check'><span class='cssd'>"+a+"</span>");
+    $(this).parent().appendTo("#com").html("<input type='checkbox' id='check' checked ><span class='cssd'>"+a+"</span>");
 });
 $('#com').on('click', '#check',function(){
     $(this).parent().appendTo("#u").html("<input type='checkbox' id='check'><span class='cssd'>"+a+"</span><input type='button' id='edit' value='Edit'><input type='button' id='delete' value=Delete>");
