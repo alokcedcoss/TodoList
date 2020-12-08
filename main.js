40c76c18275d1259564de00cc65ef4f5375948b1
alokcedcoss <74813756+alokcedcoss@users.noreply.github.com>
2020-12-08 15:06:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function()
 {
   $("#btn1").click(function()
@@ -8,21 +9,33 @@ if($("#t1").val()=='')
 }
 else
 {
+
+
 var a=$("#t1").val();
 $("#t1").val('');
     $("ul").append("<li><input type='checkbox' id='check'><span class='cssd'>"+a+"</span><input type='button' id='edit' value='Edit'><input type='button' id='delete' value=Delete></li>");
+
+
 $('#u').on('click', '#edit', function(){
     $(this).prev().attr('contenteditable','true').focus();
+ 
+
 });
 }
 $('#u').on('click', '#delete',function(){
     $(this).parent().remove();
 });
 $('#u').on('click', '#check',function(){
-    $(this).parent().appendTo("#com").html("<input type='checkbox' id='check' checked ><span class='cssd'>"+a+"</span>");
+    $(this).parent().appendTo("#com");
+   
 });
 $('#com').on('click', '#check',function(){
-    $(this).parent().appendTo("#u").html("<input type='checkbox' id='check'><span class='cssd'>"+a+"</span><input type='button' id='edit' value='Edit'><input type='button' id='delete' value=Delete>");
+    $(this).parent().appendTo("#u");
+ 
 });
+
+
+
+
   });
 });
