2eb7ee47c50187555354991ccc43e88f8dcc74b4
alokcedcoss <74813756+alokcedcoss@users.noreply.github.com>
2020-12-08 13:39:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function()
+{
+  $("#btn1").click(function()
+  {
+if($("#t1").val()=='')
+{
+	alert("enter the Task ");
+}
+else
+{
+var a=$("#t1").val();
+$("#t1").val('');
+    $("ul").append("<li><input type='checkbox' id='check'><span class='cssd'>"+a+"</span><input type='button' id='edit' value='Edit'><input type='button' id='delete' value=Delete></li>");
+$('#u').on('click', '#edit', function(){
+    $(this).prev().attr('contenteditable','true').focus();
+});
+}
+$('#u').on('click', '#delete',function(){
+    $(this).parent().remove();
+});
+$('#u').on('click', '#check',function(){
+    $(this).parent().appendTo("#com").html("<input type='checkbox' id='check'><span class='cssd'>"+a+"</span>");
+});
+$('#com').on('click', '#check',function(){
+    $(this).parent().appendTo("#u").html("<input type='checkbox' id='check'><span class='cssd'>"+a+"</span><input type='button' id='edit' value='Edit'><input type='button' id='delete' value=Delete>");
+});
+  });
+});
